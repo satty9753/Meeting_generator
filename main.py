54a4df3bf8ffe5bd517957ef45ac44ad3aa6ca74
Satty <satty9753@gmail.com>
2020-10-08 23:19:03 +0800

    parse plain text to json(one file)

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 from docx import Document
+import json
+import os
+
+path = "/Users/satty/Documents/Tools/Meeting_generator/json"
+
+files = os.listdir(path)
+
+first_file = files[0]
 
 doc = Document('SD-App 20201008.docx')
 
 numTables = doc.tables
 table = numTables[0]
 
+#會議日期
+first_filename = first_file.split('.')[0]
+date = first_file.split('SD-App')[1].split('.')[0].strip()
+table.cell(1, 1).text = date
+
+#主席
+#......
+
+#紀錄
 table.cell(3, 3).text = "Michelle"
 
-doc.save('SD-App 20201008.docx')
+######Leader
+#名稱
+#工作項目
+
+######william
+#名稱
+#工作項目
+
+
+######michelle
+#名稱
+#工作項目
+
+######sandy
+#名稱
+#工作項目
+
+
+######eric
+#名稱
+#工作項目
+
+doc.save(first_filename+'.docx')
 # table.cell(3, 4).text = "Michelle"
 
 # for table in numTables:
--- parse_content.py
+++ parse_content.py
@@ -1,24 +1,78 @@
 import os
+import json
 
-path = "/Users/michelle/Documents/IIT/Project/Tools/meeting_generator/plainText"
+path = "/Users/satty/Documents/Tools/Meeting_generator/plainText"
 
 files = os.listdir(path)
 
 first_file = files[0]
 
+print(first_file)
+
 f = open(path + '/' +first_file, 'r')
 
-data = {}
+data = {'marc':'', 'william':'', 'michelle':'', 'sandy':'', 'eric':''}
 
-for line in f.readlines():
-    if 'Marc Liu' in line:
-        if 'marc' not in data:
-            data['marc'] = ''
-        else:   
-            data['marc'] += line
+marc = 'Marc Liu'
+william = 'William Weng'
+michelle = 'Michelle Chen'
+sandy = 'Sandy Yeh'
+eric = 'Eric Yeh'
+
+def addWord(s):
+    data['marc'] += s
+    data['william'] += s
+    data['michelle'] += s
+    data['sandy'] += s
+    data['eric'] += s
+
+
+def parse_person(part):
+    if part == weeklyComplete:
+        addWord('本週完成項目')
+    elif part == processing:
+        addWord('進行中項目')
+    else:
+        addWord('下週預計')
+    data['marc'] += part.split(marc)[1].split(william)[0]
+    data['william'] += part.split(william)[1].split(michelle)[0]
+    data['michelle'] += part.split(michelle)[1].split('Android')[0]
+    data['sandy'] += part.split(sandy)[1].split(eric)[0]
+    data['eric'] += part.split(eric)[1]
+
+content = f.read()
+
+weeklyComplete = content.split('本週完成項目：')[1].split('進行中項目：')[0]
+processing = content.split('進行中項目：')[1].split('下週預計：')[0]
+futurePlan = content.split('下週預計：')[1]
+
+
+# for line in f.readlines():
+#     if '本週完成項目' in line:
+#         addWord(line)
+#     elif 'Marc Liu' in line:
+#         data['marc'] += 'Marc Liu'
+#     else:
+#         data['marc'] += line
+#         if 'William Weng' in line:
+#             break
+    # if 'Marc Liu' in line:
+    #     if 'marc' not in data:
+    #         data['marc'] = ''
+    #     else:   
+    #         data['marc'] += line
     # elif 'William' in line:
     #     data['william'] += line
+parse_person(weeklyComplete)
+parse_person(processing)
+parse_person(futurePlan)
 
 
-print(data)
 f.close()
+
+json_data = json.dumps(data,ensure_ascii=False,indent=2)
+
+fname = first_file.split('.')[0]
+
+with open(fname+'.json', "w") as outfile: 
+    outfile.write(json_data) 
